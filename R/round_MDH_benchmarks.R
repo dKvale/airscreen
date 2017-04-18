b3b6f6fd3fba628739535854d6bc35a84421ed30
dkvale <kvaled@gmail.com>
2017-04-18 17:48:48 -0500

    leave 2 sig-figs for dioxin/furans and PAH risk values

--- R/round_MDH_benchmarks.R
+++ R/round_MDH_benchmarks.R
@@ -14,15 +14,15 @@ refs      <- read_csv("data/air_benchmark_references.csv")
 
 
 # Join
-risks   <- left_join(risks, refs)
+risks   <- left_join(unrounded, refs)
 
 
 # Round MDH cancer values
-mdh <- filter(risks, `Cancer IHB Reference` %in% c("MDH","HRV","HBV","MPCA","MPCA/MDH"))
+mdh <- filter(risks[ , -c(1,3:5)], `Cancer IHB Reference` %in% c("MDH","HRV","HBV")) #"MPCA","MPCA/MDH"))
 
 
 risks_1 <- risks %>%
-           mutate(`Chronic cancer risk of 1E-5 Air Conc (ug/m3)` = ifelse(`Cancer IHB Reference` %in% c("MDH","HRV","HBV","MPCA","MPCA/MDH"), 
+           mutate(`Chronic cancer risk of 1E-5 Air Conc (ug/m3)` = ifelse(`Cancer IHB Reference` %in% c("MDH","HRV","HBV"), #"MPCA","MPCA/MDH"), 
                                                                           signif(`Chronic cancer risk of 1E-5 Air Conc (ug/m3)`, 1), 
                                                                           `Chronic cancer risk of 1E-5 Air Conc (ug/m3)`))
 
@@ -37,5 +37,17 @@ risks_1[risks_1$Pollutant == "Benzene", ]$`Chronic cancer risk of 1E-5 Air Conc 
 write_csv(risks_1[ , 1:6], "data/air_benchmarks.csv")
 
 
+# Join to RASS table for copy/pasting
+rass <- read_excel("X:/Agency_Files/Outcomes/Risk_Eval_Air_Mod/_Air_Risk_Evaluation/AERA program/RASS/AERA RASS (aq9-22-7) - April 2017.xlsx", 7, skip = 18)
+
+rass <- rass[ , -c(1,2)]
+
+names(rass)[1] <- "CAS"
+
+rass$CAS <- str_trim(rass$CAS)
+
+new_rass <- left_join(rass[ , -c(3,4,6:12,14:17,19:24,26:32)], risks_1[ , 1:6])
+
+write_csv(new_rass, "../Rounded IHBs for RASS spreadsheet.csv")
 
 ##
